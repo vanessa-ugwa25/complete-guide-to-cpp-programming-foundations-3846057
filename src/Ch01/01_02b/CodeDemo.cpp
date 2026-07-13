a482df65efa71ebaa98d2cd5def0a675fb8522a1
vanessa-ugwa25 <vanessa.ugwa25@tamu.edu>
2026-07-13 21:55:27 +0000

    edited with linked in tutor

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+//pre - processor directive (do not end with a semicolon!!), #include & #define? '#' is an important operator
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+    std::cout << std::endl << std::endl; //making hella gaps between code & debugger report
+    //std::cout << "My name is Vanessa, I am from Latvia" << std::endl;
+    return 0;
+}
