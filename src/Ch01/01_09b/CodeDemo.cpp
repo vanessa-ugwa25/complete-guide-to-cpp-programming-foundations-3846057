46c1ced418556da2445c7b640a5bb08ca8abbc8d
vanessa-ugwa25 <vanessa.ugwa25@tamu.edu>
2026-07-15 03:05:35 +0000

    ch2_04 done

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -13,4 +13,4 @@ int main(){
     
     std::cout << std::endl << std::endl;
     return 0;
-}
+} 
--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,7 +4,26 @@
 
 #include <iostream>
 
+int a, b = 5;//same indentation as main function: making a & b GLOBAL variables*****
+
 int main(){
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+     std::cout << std::endl << std::endl;
+    
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    unsigned int positive; 
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
